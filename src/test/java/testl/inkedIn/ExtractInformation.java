e8befa5d7c7246acc6d467301940b96bbcd16204
vigneshgivi <vigneshgivi27@gmail.com>
2024-08-07 22:20:46 +0530

    first commit

--- src/test/java/testl/inkedIn/ExtractInformation.java
+++ src/test/java/testl/inkedIn/ExtractInformation.java
@@ -1,6 +1,7 @@
 package testl.inkedIn;
 
 import java.awt.AWTException;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,146 +10,221 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.DataProvider;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import utility.ExcelUtility;
 
-public class ExtractInformation extends Appbase {
+public class ExtractInformation {
 
-	Map<String, String> keyValuePairs;
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private Actions actions;  // Declare Actions
 
-	public ExtractInformation() {
-		// Constructor
-	}
+    private Map<String, String> keyValuePairs;
 
-//	@DataProvider(name = "loginData")
-//	public Object[][] loginData() {
-//		return new Object[][] { { "vigneshvasugv01@gmail.com", "Linkedin@user@10" },
-//				{ "vigneshgivi27@gmail.com", "Linkedin@0708#" } };
-//	}
-	
-	String email = "vigneshgivi27@gmail.com";
-	String password ="Linkedin@0708#";
-	
+    // LinkedIn credentials
+    private String linkedinEmail = "vigneshgivi27@gmail.com";
+    private String linkedinPassword = "Linkedin@0708#";
 
-//	@Test(priority = 1, dataProvider = "loginData")
-	
-	@Test(priority =1)
-	public void login() throws InterruptedException {
-		// Login logic
-		driver.findElement(By.xpath("//input[@id='username']")).sendKeys(email);
-		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		driver.navigate().to("https://www.linkedin.com/jobs/");
+    // Naukri credentials
+    private String naukriEmail = "vigneshgivi27@gmail.com";
+    private String naukriPassword = "Naukri@27";
 
-	}
+    @BeforeClass
+    public void setUp() {
+        // Set the path of the ChromeDriver
+        // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
 
-	@Test(priority = 2)
-	public void selectFilter() throws AWTException, InterruptedException {
-		Thread.sleep(10000);
-		driver.findElement(By.xpath("(//input[contains(@id,'jobs-search-box-keyword-id')])")).sendKeys("Test Engineer",
-				Keys.ENTER);
+        // Create an instance of the ChromeDriver
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20)); // Increased wait time for better handling of delays
+        actions = new Actions(driver);  // Initialize Actions
 
-		Actions actions = new Actions(driver);
-		driver.findElement(By.xpath("//button[text()='Date posted']")).click();
-		Thread.sleep(5000);
+        // Maximize the browser window
+        driver.manage().window().maximize();
+    }
 
-		WebElement mouseHoverToPast24hours = driver.findElement(By.xpath("//span[text()='Past 24 hours']"));
-		actions.moveToElement(mouseHoverToPast24hours).click().build().perform();
-		Thread.sleep(5000);
+    @Test(priority = 1)
+    public void linkedinLoginAndFilter() throws InterruptedException, AWTException {
+        driver.get("https://www.linkedin.com/login");
+        driver.findElement(By.xpath("//input[@id='username']")).sendKeys(linkedinEmail);
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys(linkedinPassword);
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.navigate().to("https://www.linkedin.com/jobs/");
 
-		WebElement mouseHoverToShowResult = driver.findElement(
-				By.xpath("((//*[@class='reusable-search-filters-trigger-dropdown__container'])//button)[2]"));
-		actions.moveToElement(mouseHoverToShowResult).click().build().perform();
-		Thread.sleep(10000);
+        Thread.sleep(10000);
 
-		WebElement mouseHoverToexperienceFilter = driver
-				.findElement(By.xpath("//button[@id='searchFilter_experience']"));
-		actions.moveToElement(mouseHoverToexperienceFilter).click().build().perform();
-		Thread.sleep(10000);
+        // Perform a search for "Test Engineer"
+        WebElement searchBox = driver.findElement(By.xpath("(//input[contains(@id,'jobs-search-box-keyword-id')])"));
+        searchBox.sendKeys("Test Engineer", Keys.ENTER);
 
-		WebElement mouseHoverToEntrylevel = driver.findElement(By.xpath("(//span[text()='Entry level'])[1]"));
-		actions.moveToElement(mouseHoverToEntrylevel).click().build().perform();
+        // Click on "Date posted" button
+        WebElement datePostedButton = driver.findElement(By.xpath("//button[text()='Date posted']"));
+        actions.moveToElement(datePostedButton).click().perform();
+        Thread.sleep(5000);
 
-		WebElement mouseHoverToAssociate = driver.findElement(By.xpath("(//span[text()='Associate'])[1]"));
-		actions.moveToElement(mouseHoverToAssociate).click().build().perform();
-		Thread.sleep(10000);
+        // Click on "Past 24 hours" filter
+        WebElement past24HoursFilter = driver.findElement(By.xpath("//span[text()='Past 24 hours']"));
+        actions.moveToElement(past24HoursFilter).click().perform();
+        Thread.sleep(5000);
 
-		WebElement mouseHoverToexperienceFilterShowResult = driver
-				.findElement(By.xpath("(//button[contains(@aria-label,'Apply current filter to show')])[2]"));
-		actions.moveToElement(mouseHoverToexperienceFilterShowResult).click().build().perform();
-		Thread.sleep(10000);
+        // Click to show results
+        WebElement showResultsButton = driver.findElement(By.xpath("((//*[@class='reusable-search-filters-trigger-dropdown__container'])//button)[2]"));
+        actions.moveToElement(showResultsButton).click().perform();
+        Thread.sleep(10000);
 
-		WebElement mouseHoverToEasyApply = driver.findElement(By.xpath("//button[text()='Easy Apply']"));
-		actions.moveToElement(mouseHoverToEasyApply).click().build().perform();
-		Thread.sleep(10000);
+        // Click on experience filter
+        WebElement experienceFilterButton = driver.findElement(By.xpath("//button[@id='searchFilter_experience']"));
+        actions.moveToElement(experienceFilterButton).click().perform();
+        Thread.sleep(10000);
 
-		System.out.println("Select Filter Completed");
-	}
+        // Click on "Entry level" and "Associate" options
+        WebElement entryLevelOption = driver.findElement(By.xpath("(//span[text()='Entry level'])[1]"));
+        actions.moveToElement(entryLevelOption).click().perform();
 
-	@Test(priority = 3)
-	public void extractList() throws InterruptedException {
-		boolean hasNextPage = true;
-		keyValuePairs = new HashMap<>();
-		while (hasNextPage) {
-			Thread.sleep(10000);
+        WebElement associateOption = driver.findElement(By.xpath("(//span[text()='Associate'])[1]"));
+        actions.moveToElement(associateOption).click().perform();
+        Thread.sleep(10000);
 
-			List<WebElement> scrollUpto = driver
-					.findElements(By.xpath("(//li[contains(@class,'scaffold-layout__list-item')])"));
+        // Apply the experience filter
+        WebElement applyExperienceFilterButton = driver.findElement(By.xpath("(//button[contains(@aria-label,'Apply current filter to show')])[2]"));
+        actions.moveToElement(applyExperienceFilterButton).click().perform();
+        Thread.sleep(10000);
 
-			for (WebElement scrollallselect : scrollUpto) {
-				Thread.sleep(2000);
-				((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", scrollallselect);
-			}
+        // Click on "Easy Apply" button
+        WebElement easyApplyButton = driver.findElement(By.xpath("//button[text()='Easy Apply']"));
+        actions.moveToElement(easyApplyButton).click().perform();
 
-			Thread.sleep(5000);
+        System.out.println("Select Filter Completed");
 
-			List<WebElement> jobsTitleList = driver
-					.findElements(By.xpath("//div[@class='full-width artdeco-entity-lockup__title ember-view']/a"));
+        extractLinkedInJobInfo();
+    }
 
-			for (WebElement jobTitle : jobsTitleList) {
-				((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", jobTitle);
+    private void extractLinkedInJobInfo() throws InterruptedException {
+        boolean hasNextPage = true;
+        keyValuePairs = new HashMap<>();
+        while (hasNextPage) {
+            Thread.sleep(10000);
 
-				String getTitle = jobTitle.getAttribute("aria-label");
-				System.out.println(getTitle);
+            List<WebElement> scrollUpto = driver.findElements(By.xpath("(//li[contains(@class,'scaffold-layout__list-item')])"));
 
-				if (getTitle.contains("Quality Assurance") || getTitle.contains("QA") || getTitle.contains("Test")
-						|| getTitle.contains("Quality Test Engineer") || getTitle.contains("Performance Testing")
-						|| getTitle.contains("SDET") || getTitle.contains("Automation Engineer")) {
-					jobTitle.click();
+            for (WebElement scrollallselect : scrollUpto) {
+                Thread.sleep(2000);
+                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", scrollallselect);
+            }
 
-					List<WebElement> hiringTeam = driver
-							.findElements(By.xpath("//div[@class='hirer-card__hirer-information']/a"));
+            Thread.sleep(5000);
 
-					if (hiringTeam.isEmpty()) {
-						System.out.println("Meet the hiring team not found");
-					} else {
-						String name = driver.findElement(By.xpath("//div[@class='hirer-card__hirer-information']/a"))
-								.getAttribute("aria-label");
-						String URL = driver.findElement(By.xpath("//div[@class='hirer-card__hirer-information']/a"))
-								.getAttribute("href");
-						System.out.println(name + " : " + URL);
+            List<WebElement> jobsTitleList = driver.findElements(By.xpath("//div[@class='full-width artdeco-entity-lockup__title ember-view']/a"));
 
-						keyValuePairs.put(name, URL);
-					}
-				}
-			}
+            for (WebElement jobTitle : jobsTitleList) {
+                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", jobTitle);
 
-			try {
-				WebElement nextButton = driver.findElement(By.xpath(
-						"(//div[contains(@class,\"jobs-search-results-list__pagination\")]//li/button[@aria-current=\"true\"]/parent::li/following-sibling::li)[1]"));
-				nextButton.click(); // Click the "Next" button if it exists
-				Thread.sleep(5000); // Wait for the next page to load
-			} catch (NoSuchElementException e) {
-				// No "Next" button found, end the loop
-				hasNextPage = false;
-				System.out.println("Extraction Completed");
-			}
+                String getTitle = jobTitle.getAttribute("aria-label");
+                System.out.println(getTitle);
 
-		}
-		ExcelUtility.writeKeyValuePairsToExcel(keyValuePairs);
-	}
+                if (getTitle.contains("Quality Assurance") || getTitle.contains("QA") || getTitle.contains("Test") || getTitle.contains("Quality Test Engineer") || getTitle.contains("Performance Testing") || getTitle.contains("SDET") || getTitle.contains("Automation Engineer")) {
+                    jobTitle.click();
+
+                    List<WebElement> hiringTeam = driver.findElements(By.xpath("//div[@class='hirer-card__hirer-information']/a"));
+
+                    if (hiringTeam.isEmpty()) {
+                        System.out.println("Meet the hiring team not found");
+                    } else {
+                        String name = driver.findElement(By.xpath("//div[@class='hirer-card__hirer-information']/a")).getAttribute("aria-label");
+                        String URL = driver.findElement(By.xpath("//div[@class='hirer-card__hirer-information']/a")).getAttribute("href");
+                        System.out.println(name + " : " + URL);
+
+                        keyValuePairs.put(name, URL);
+                    }
+                }
+            }
+
+            try {
+                WebElement nextButton = driver.findElement(By.xpath("(//div[contains(@class,\"jobs-search-results-list__pagination\")]//li/button[@aria-current=\"true\"]/parent::li/following-sibling::li)[1]"));
+                nextButton.click(); // Click the "Next" button if it exists
+                Thread.sleep(5000); // Wait for the next page to load
+            } catch (NoSuchElementException e) {
+                // No "Next" button found, end the loop
+                hasNextPage = false;
+                System.out.println("Extraction Completed");
+            }
+        }
+        ExcelUtility.writeKeyValuePairsToExcel(keyValuePairs);
+    }
+
+    @Test(priority = 2)
+    public void naukriLoginAndUpdateResumeHeadline() throws InterruptedException {
+        driver.get("https://www.naukri.com/");
+        
+        WebElement loginButtonInitial = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(text(),'Login')]")));
+        loginButtonInitial.click();
+
+        WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='form-row']/input)[1]")));
+        usernameField.sendKeys(naukriEmail);
+
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='form-row']/input)[2]")));
+        passwordField.sendKeys(naukriPassword);
+
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Login']")));
+        loginButton.click();
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='view-profile-wrapper']")));
+
+        WebElement viewProfile = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='view-profile-wrapper']")));
+        viewProfile.click();
+
+        try {
+            WebElement cancelIcon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class,'crossIcon chatBot')]")));
+            cancelIcon.click();
+        } catch (Exception e) {
+            System.out.println("Cancel icon not found or not clickable.");
+        }
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, -500);"); // Adjust scroll amount as needed
+
+        Thread.sleep(2000); // Sleep for 2 seconds
+
+        WebElement editResumeHeadlineIcon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[@class='edit icon'])[1]")));
+        editResumeHeadlineIcon.click();
+
+        Thread.sleep(2000); // Sleep for 2 seconds
+
+        WebElement resumeHeadlineTextArea = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@id='resumeHeadlineTxt']")));
+        String existingText = resumeHeadlineTextArea.getText().trim();
+
+        System.out.println("Existing text: \"" + existingText + "\"");
+
+        if (existingText.endsWith(".")) {
+            existingText = existingText.substring(0, existingText.length() - 1); // Remove trailing period
+            System.out.println("Removed trailing period.");
+        } else {
+            existingText += "."; // Add a period if not present
+            System.out.println("Added trailing period.");
+        }
+
+        resumeHeadlineTextArea.clear();
+        resumeHeadlineTextArea.sendKeys(existingText);
+
+        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Save']")));
+        saveButton.click();
+
+        System.out.println("Save button clicked.");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
